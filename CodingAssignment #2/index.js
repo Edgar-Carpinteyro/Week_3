7238c5ccf2971025d5080bf26a6934c96b293471
Edgar Carpinteyro <110205691+Edgar-Carpinteyro@users.noreply.github.com>
2022-08-27 17:02:14 -0700

    Add files via upload

---
+++ CodingAssignment #2/index.js
@@ -0,0 +1,27 @@
+/*Inside index.js, create one variable for each of the following real-life examples and
+assign them values: 
+*/
+
+let itemPrice = 100;
+let moneyInWallet = 150;
+let numberOfFriends = 2;
+let ageInYears = 30;
+let firstName = "Edgar";
+let lastName = "Carpinteyro";
+let middleInitial = "R";
+
+if (moneyInWallet > itemPrice) {
+    console.log(moneyInWallet - itemPrice);
+}
+
+//Number of friends you've made each year based on your age variable and your number of friends variable.
+
+if (numberOfFriends = ageInYears / numberOfFriends); {
+    console.log(numberOfFriends);
+}
+
+//Full name based on first name, middle initial, and last name.
+let fullName;
+if (fullName = `${ firstName } ${ middleInitial } ${ lastName }`); {
+    console.log(fullName);
+}
